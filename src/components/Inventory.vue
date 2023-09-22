b009f3947357c6dae05674c88db5952f795f4d1c
Sakamitsu <petrovichwot7@gmail.com>
2023-09-22 22:23:06 +0300

    Add span number to table

--- src/components/Inventory.vue
+++ src/components/Inventory.vue
@@ -5,8 +5,14 @@
   <div class="inventory">
     <table>
       <tr>
-        <td>1</td>
-        <td>2</td>
+        <td>
+          1
+          <span>4</span>
+        </td>
+        <td>
+          2
+          <span>5</span>
+        </td>
         <td>3</td>
         <td>4</td>
         <td>5</td>
@@ -59,9 +65,23 @@
       border-collapse: collapse;
       border-style: hidden;
       & td, th {
+        position: relative;
         text-align: center;
         color: white;
         border: 2px solid #4D4D4D;
+        span {
+          position: absolute;
+          bottom: 0px;
+          right: 0px;
+          width: 16px;
+          height: 16px;
+          border-top: 2px solid #4D4D4D;
+          border-left: 2px solid #4D4D4D;
+          border-top-left-radius: 6px;
+          padding: 2px;
+          color: #7D7D7D;
+          font-family: "Inter";
+        }
       }
     }
   }
