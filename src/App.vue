868d433268aa4786987373651c934cb008db5cb1
Sakamitsu <petrovichwot7@gmail.com>
2023-09-22 22:01:47 +0300

    Add Inventory style

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,37 @@
 <script setup lang="ts">
   import Menu from './components/Menu.vue'
+  import Inventory from './components/Inventory.vue'
 </script>
 
 <template>
-  <Menu/>
+  <div class="app-container">
+    <div class="row-container">
+      <Menu/>
+      <Inventory/>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
+  .app-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position:fixed;
+    /* position: absolute; */
+    padding:0;
+    margin:0;
+    top:0;
+    left:0;
+
+    width: 100%;
+    height: 100%;
+    background: #1E1E1E;
+    & .row-container {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      gap: 24px;
+    }
+  }
 </style>
---
+++ src/components/Inventory.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="inventory">
+    <table>
+      <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+        <td>4</td>
+        <td>5</td>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+        <td>4</td>
+        <td>5</td>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+        <td>4</td>
+        <td>5</td>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+        <td>4</td>
+        <td>5</td>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+        <td>4</td>
+        <td>5</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .inventory {
+    width: 525px;
+    height: 500px;
+    background-color: bisque;
+    border: 2px solid #4D4D4D;
+    border-radius: 12px;
+    background-color: #262626;
+
+    & table {
+      height: 100%;
+      width: 100%;
+      margin: 0 auto;
+      border-collapse: collapse;
+      border-style: hidden;
+      & td, th {
+        text-align: center;
+        color: white;
+        border: 2px solid #4D4D4D;
+      }
+    }
+  }
+</style>
